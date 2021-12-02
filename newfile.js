cb0847942fafedf042befafb7589231cd5765568
Oscar Kimani <okimani7@gmail.com>
2021-12-02 12:53:30 +0300

    arithmetic js

--- newfile.js
+++ newfile.js
@@ -5,3 +5,21 @@ console.log(1*3);
 console.log(9/2);
 console.log(7+8*9);
 console.log((7+8)*9);
+
+// Add 
+console.log(89*54);
+console.log(932*1900);
+console.log(-34*8);
+console.log(25*700);
+
+// Substract
+console.log(665-99);
+
+//Multiple
+console.log(89*54);
+
+// Divide
+console.log(1008/7);
+
+
+
